fec2b89bb1faa8b436d95c8e18273a862ec6182a
auvy09 <akibalmahmud356@gmail.com>
2024-01-29 14:49:19 +0600

    first commit

--- src/sections/overview/app-widget-summary.jsx
+++ src/sections/overview/app-widget-summary.jsx
@@ -5,34 +5,47 @@ import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
-import { fShortenNumber } from 'src/utils/format-number';
+// import { fShortenNumber } from 'src/utils/format-number';
+
 
 // ----------------------------------------------------------------------
 
-export default function AppWidgetSummary({ title, total, icon, color = 'primary', sx, ...other }) {
-  return (
+export default function AppWidgetSummary({ title, total, icon,time ,color = 'primary', sx, ...other }) {
+  return (<>
     <Card
       component={Stack}
-      spacing={3}
-      direction="row"
+      
+      direction="column"
       sx={{
-        px: 3,
-        py: 5,
-        borderRadius: 2,
+        px: 1,
+        py: 2,
+       borderBottomRightRadius:0,
+       borderBottomLeftRadius:0,
         ...sx,
+        alignItems: 'center',
+        justifyContent: 'center'
       }}
       {...other}
     >
-      {icon && <Box sx={{ width: 64, height: 64 }}>{icon}</Box>}
-
-      <Stack spacing={0.5}>
-        <Typography variant="h4">{fShortenNumber(total)}</Typography>
-
-        <Typography variant="subtitle2" sx={{ color: 'text.disabled' }}>
-          {title}
+      {icon && <Box sx={{ width: ['100px'], height: ['120px'], mt:3, mb:6 }} >{icon}</Box>}
+     
+        <Typography gutterBottom  variant="h6" component="div">
+          Total Complain
         </Typography>
-      </Stack>
+        <Typography   variant="h6" component="div">
+          {total}
+        </Typography>
+        
+      
     </Card>
+    <Box sx={{
+        
+       borderBottomRightRadius:2,
+       borderBottomLeftRadius:2,
+        ...sx,
+        textAlign:'center',
+       }}
+         bgcolor={['rgba(4, 152, 74, 0.1)']}> {time}</Box></>
   );
 }
 
@@ -42,4 +55,5 @@ AppWidgetSummary.propTypes = {
   sx: PropTypes.object,
   title: PropTypes.string,
   total: PropTypes.number,
+  time:PropTypes.string,
 };
--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -2,6 +2,8 @@ import { faker } from '@faker-js/faker';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
+import { Box, Button, Input } from '@mui/material';
+
 import Typography from '@mui/material/Typography';
 
 import Iconify from 'src/components/iconify';
@@ -16,107 +18,132 @@ import AppTrafficBySite from '../app-traffic-by-site';
 import AppCurrentSubject from '../app-current-subject';
 import AppConversionRates from '../app-conversion-rates';
 
+
+
+
 // ----------------------------------------------------------------------
 
 export default function AppView() {
   return (
     <Container maxWidth="xl">
-      <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi, Welcome back 👋
+     <Box> <Typography color={['#048943']} variant="h4" sx={{ mb: 5 }}>
+        All Utilities
       </Typography>
-
-      <Grid container spacing={3}>
-        <Grid xs={12} sm={6} md={3}>
+      <Box mb={3} >From: <Input type='date'/> To: <Input type='date'/>  <Button variant="contained" color="success">
+ Filter
+</Button> </Box>
+     </Box>
+      <Grid container spacing={1}>
+        <Grid xs={6} sm={2} md={2}>
           <AppWidgetSummary
             title="Weekly Sales"
             total={714000}
             color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
+            icon={<img alt="icon" src="/public/assets/logo/logo_1.png"/>}
+            time="10:22 am 01, Jan 24"
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={6} sm={2} md={2}>
           <AppWidgetSummary
             title="New Users"
             total={1352831}
             color="info"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
+            icon={<img alt="icon" src="/public/assets/logo/logo_2.png" />}
+            time="10:22 am 01, Jan 24"
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={6} sm={2} md={2}>
           <AppWidgetSummary
             title="Item Orders"
             total={1723315}
             color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+            icon={<img alt="icon" src="/public/assets/logo/logo_3.png" />}
+            time="10:22 am 01, Jan 24"
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={6} sm={2} md={2}>
+          <AppWidgetSummary
+            title="Bug Reports"
+            time="10:22 am 01, Jan 24"
+            total={234}
+            color="error"
+            icon={<img alt="icon" src="/public/assets/logo/logo_4.png" />}
+          />
+        </Grid>
+        <Grid xs={6} sm={2} md={2}>
           <AppWidgetSummary
             title="Bug Reports"
             total={234}
             color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
+            icon={<img alt="icon" src="/public/assets/logo/logo_5.png" />}
+            time="10:22 am 01, Jan 24"
           />
         </Grid>
-
+        <Grid xs={6} sm={2} md={2}>
+          <AppWidgetSummary
+          
+            title="Bug Reports"
+            total={234}
+            color="error"
+            icon={<img  alt="icon" src="/public/assets/logo/logo_6.png" />}
+            time="10:22 am 01, Jan 24"
+          />
+        </Grid>
+<Grid xs={12} md={6} lg={4}>
+          <AppCurrentVisits
+            title="Total Tickets"
+            chart={{
+              series: [
+                { label: 'Opened', value: 3573 },
+                { label: 'Solved', value: 5574 },
+                { label: 'Processing', value: 1536 },
+                { label: 'Reopened', value: 300 },
+              ],
+            }}
+          />
+        </Grid>
         <Grid xs={12} md={6} lg={8}>
           <AppWebsiteVisits
-            title="Website Visits"
-            subheader="(+43%) than last year"
+            title="Utility Wise Tickets"
+            subheader="(Opened and Solved)"
             chart={{
               labels: [
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
+                'BPDB',
+                'BREB',
+                'DESCO',
+                'DPDC',
+                'WZPDCL',
+                'NESCO',
+                
               ],
               series: [
                 {
                   name: 'Team A',
                   type: 'column',
                   fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                  data: [23, 11, 22, 27, 13, 22],
                 },
                 {
                   name: 'Team B',
                   type: 'area',
                   fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+                  data: [44, 55, 41, 67, 22, 43],
                 },
                 {
                   name: 'Team C',
                   type: 'line',
                   fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                  data: [30, 25, 36, 30, 45, 35],
                 },
               ],
             }}
           />
         </Grid>
 
-        <Grid xs={12} md={6} lg={4}>
-          <AppCurrentVisits
-            title="Current Visits"
-            chart={{
-              series: [
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ],
-            }}
-          />
-        </Grid>
+        
 
         <Grid xs={12} md={6} lg={8}>
           <AppConversionRates
